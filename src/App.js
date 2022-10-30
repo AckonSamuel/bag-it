7da9c0583745353ef47ded95030901e1e14951ec
Samuel Ackon <ackons433@gmail.com>
2022-10-30 21:46:17 +0000

    Refactor code to include hover features

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import LinksPage from './components/linkspage/LinksPage';
 import Footer from './components/footer/Footer';
 import share from './assets/images/share_button.png';
 import d_share from './assets/images/desk_share.png';
+import camera from './assets/images/camera.png';
+import s_share from './assets/images/Tooltip.png';
 import './App.css';
 
 const App = () => 
     <div className="App">
-      <img src={d_share} alt="share" className="d-share" /> <img src={share} alt="share" className="share" />
+     <img src={d_share} alt="share" className="d-share" />
+     <img src={s_share} alt="share" className="s-share" /> 
+      <img src={share} alt="share" className="share" />
       <div className='profile-container'>
       <div id="profile_img">
+        <div className='camera'><img src={camera} alt="camera" className='cam-in'/></div>
       </div>
       <h2 id="annette">Ackon Samuel</h2>
        <small id='slack'><em>Slack: @ackon_samuel</em></small> 
