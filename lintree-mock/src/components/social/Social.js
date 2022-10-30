da65bf588927b6621e27fd5973a15aa46e6f049f
Samuel Ackon <ackons433@gmail.com>
2022-10-30 14:29:40 +0000

    Refactor code on social component

--- lintree-mock/src/components/social/Social.js
+++ lintree-mock/src/components/social/Social.js
@@ -1,13 +1,12 @@
-const Social = () => {
-    const images = []
+import slack from './../../assets/images/slack.png';
+import github from './../../assets/images/github.png';
+import './../../assets/stylesheets/social.css'
 
-    return (
-        <div className='social-group'>
-            {
-                images.map((image) => <a><img src={image} alt={image} /></a>)
-            }
-        </div>
-    )
-}
+const Social = () =>
+<div className='socials'>
+   <a href="#"><img src={slack} alt='slack'/></a>
+   <a href="https://www.github.com/AckonSamuel/"><img src={github} alt='slack'/></a>
+</div>;
+
 
 export default Social;
